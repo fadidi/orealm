6ca4bdffaac971cc6d739e541cb30fdac61e6648
John F. Brown <jack@brownjohnf.com>
2012-09-21 16:03:17 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Overrealm::Application.config.secret_token = 'cffa59862322470f7ceef65562f8d53bd6d32a3a282887cc012255f60158ff3ee5d83beb9eda3b24882ba30063ebc0fabb3ed2eb2d87885db5db2db0d009c7f3'
