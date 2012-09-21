682d0a34a34523ff6ae0d07f714cb3455877eb4a
John F. Brown <jack@brownjohnf.com>
2012-09-21 22:30:27 +0000

    added searchbox/elasticsearch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
+
   rolify
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
